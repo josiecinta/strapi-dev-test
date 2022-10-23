c60ca3c10ae11a38689701cba5cbf30e62456eb5
Wijayaac <wijayaac25@gmail.com>
2022-10-23 10:25:51 +0800

    Add : postgres sql connection configuration

--- config/database.js
+++ config/database.js
@@ -1,13 +1,16 @@
 module.exports = ({ env }) => ({
   connection: {
-    client: "mysql",
+    client: "postgres",
     connection: {
       host: env("DATABASE_HOST", "127.0.0.1"),
-      port: env.int("DATABASE_PORT", 3306),
+      port: env.int("DATABASE_PORT", 5432),
       database: env("DATABASE_NAME"),
       user: env("DATABASE_USERNAME"),
       password: env("DATABASE_PASSWORD"),
-      ssl: env.bool("DATABASE_SSL", true),
+      ssl: {
+        rejectUnauthorized: env.bool("DATABASE_SSL_SELF", false),
+      },
     },
+    debug: false,
   },
 });
