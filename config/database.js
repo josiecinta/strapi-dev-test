422691d73bb0fe2c421faa632b60181ed73b4b59
Wijayaac <wijayaac25@gmail.com>
2022-10-23 10:52:31 +0800

    Add : configuration for mysql database

--- config/database.js
+++ config/database.js
@@ -1,15 +1,13 @@
 module.exports = ({ env }) => ({
   connection: {
-    client: "postgres",
+    client: "mysql",
     connection: {
       host: env("DATABASE_HOST", "127.0.0.1"),
-      port: env.int("DATABASE_PORT", 5432),
+      port: env.int("DATABASE_PORT", 3306),
       database: env("DATABASE_NAME"),
       user: env("DATABASE_USERNAME"),
       password: env("DATABASE_PASSWORD"),
-      ssl: {
-        rejectUnauthorized: env.bool("DATABASE_SSL_SELF", false),
-      },
+      ssl: env.bool("DATABASE_SSL", false),
     },
     debug: false,
   },
